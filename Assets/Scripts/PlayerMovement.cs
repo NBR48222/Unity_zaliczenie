1a9efabec8df83ec531c4beeed71534f67338ef7
NBR_4822 <101012866+NBR48222@users.noreply.github.com>
2023-06-16 23:04:13 +0200

    V1.0.1
    -Improved player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,33 +1,21 @@
+    
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Szybkość poruszania gracza
-    public float moveSpeed = 5f;
-
-    // Granice ekranu
-    private float minX, maxX, minY, maxY;
-
-    public GameObject laserPrefab;  // Prefabrykat lasera
-    public Transform firePoint;    // Punkt startowy dla wystrzałów
-    public float fireRate = 1f;    // Częstotliwość strzałów (na sekundę)
+    public float moveSpeed = 5f; // Szybkość poruszania gracza
     public int numberOfLasers = 1; // Liczba wystrzałów na raz
+    public float fireRate = 1.2f;    // Częstotliwość strzałów (na sekundę)
+    public Transform firePoint;    // Punkt startowy dla wystrzałów
+    public GameObject laserPrefab;  // Prefabrykat lasera
     private float nextFireTime = 0f;  // Czas, kiedy będzie możliwe kolejne strzały
     private int playerLevel = 1; //Poziom gracza
 
     private void Start()
     {
-        // Ustalenie granic ekranu na podstawie rozmiaru kamery
-        float camDistance = Vector3.Distance(transform.position, Camera.main.transform.position);
-        Vector2 bottomLeft = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, camDistance));
-        Vector2 topRight = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, camDistance));
 
-        minX = bottomLeft.x + transform.localScale.x / 2;
-        maxX = topRight.x - transform.localScale.x / 2;
-        minY = bottomLeft.y + transform.localScale.y / 2;
-        maxY = topRight.y - transform.localScale.y / 2;
     }
 
     private void Update()
@@ -40,19 +28,17 @@ public class PlayerMovement : MonoBehaviour
             // Sprawdzenie rodzaju dotyku
             if (touch.phase == TouchPhase.Moved)
             {
-                // Pobranie zmiany pozycji dotyku
-                Vector2 touchDeltaPosition = touch.deltaPosition;
+                // Pobranie pozycji dotyku w przestrzeni ekranu
+                Vector3 touchPosition = touch.position;
 
-                // Obliczenie nowej pozycji gracza
-                Vector3 newPosition = transform.position + new Vector3(touchDeltaPosition.x, touchDeltaPosition.y, 0) * moveSpeed * Time.deltaTime;
+                // Przeliczenie pozycji dotyku na pozycję w przestrzeni gry
+                Vector3 worldTouchPosition = Camera.main.ScreenToWorldPoint(touchPosition);
 
-                // Ograniczenie pozycji gracza do granic ekranu
-                newPosition.x = Mathf.Clamp(newPosition.x, minX, maxX);
-                newPosition.y = Mathf.Clamp(newPosition.y, minY, maxY);
-
-                // Ustawienie nowej pozycji gracza
-                transform.position = newPosition;
+                // Ustalenie nowej pozycji gracza na podstawie pozycji dotyku
+                transform.position = new Vector3(worldTouchPosition.x, worldTouchPosition.y, transform.position.z);
             }
+
+
         }
 
         if (Input.touchCount > 0 && Time.time >= nextFireTime)
@@ -88,6 +74,5 @@ public class PlayerMovement : MonoBehaviour
     {
         Destroy(buff); // Zniszczenie buffa
         playerLevel++; // Zwiększenie poziomu gracza
-                       // Dodatkowe działania po zebraniu buffa
     }
 }
